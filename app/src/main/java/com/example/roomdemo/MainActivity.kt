676812b33cf786ac6f480a87f4709d4840178a56
DESKTOP-4L7GJRP\Orax <saad.oraxtech@gmail.com>
2023-12-21 13:23:40 +0500

    Data is added and visisble

--- app/src/main/java/com/example/roomdemo/MainActivity.kt
+++ app/src/main/java/com/example/roomdemo/MainActivity.kt
@@ -2,9 +2,13 @@ package com.example.roomdemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Adapter
 import android.widget.Toast
 import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.roomdemo.databinding.ActivityMainBinding
+import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
@@ -19,6 +23,13 @@ class MainActivity : AppCompatActivity() {
         binding?.btnAdd?.setOnClickListener{
             addRecord(employeeDao)
         }
+
+        lifecycleScope.launch {
+            employeeDao.fetchAllEmployees().collect(){
+                val list = ArrayList(it)
+                setUpListDataIntoRecyclerViewer(list,employeeDao)
+            }
+        }
     }
 
     private fun addRecord(employeeDao: EmployeeDao){
@@ -32,7 +43,24 @@ class MainActivity : AppCompatActivity() {
                 binding?.etEmailId?.text?.clear()
             }
         }else{
-            Toast.makeText(applicationContext, "NAME AND EMAIL CANNOT BE BLANKED!", Toast.LENGTH_LONG).show()
+            Toast.makeText(applicationContext, "Enter Name and Email!", Toast.LENGTH_LONG).show()
+        }
+    }
+
+    private fun setUpListDataIntoRecyclerViewer
+                (employeesList:ArrayList<EmployeeEntity>,
+                 employeeDao: EmployeeDao){
+        if (employeesList.isNotEmpty()){
+            val itemAdaptor = ItemAdaptor(employeesList)
+
+            binding?.rvItemsList?.layoutManager= LinearLayoutManager(this)
+            binding?.rvItemsList?.adapter = itemAdaptor
+            binding?.rvItemsList?.visibility = View.VISIBLE
+            binding?.tvNoRecordsAvailable?.visibility = View.GONE
+        }
+        else{
+            binding?.rvItemsList?.visibility = View.GONE
+            binding?.tvNoRecordsAvailable?.visibility= View.VISIBLE
         }
     }
 }
